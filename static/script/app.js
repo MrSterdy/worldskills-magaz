72f3d0938c158157d777f8c2e8d22e254f8c5635
MrSterdy <mrsterdy@gmail.com>
2024-01-07 14:30:14 +0300

    Seed products

--- static/script/app.js
+++ static/script/app.js
@@ -19,6 +19,25 @@ class ProductRepository {
     getAll() {
         return JSON.parse(localStorage.getItem(ProductRepository.#KEY) ?? "[]");
     }
+
+    clear() {
+        localStorage.removeItem(ProductRepository.#KEY);
+    }
+
+    /**
+     * @param {Product} product
+     */
+    add(product) {
+        const products = this.getAll();
+
+        if (products.some(p => product.id === p.id)) {
+            return;
+        }
+
+        products.push(product);
+
+        localStorage.setItem(ProductRepository.#KEY, JSON.stringify(products));
+    }
 }
 
 const productRepository = new ProductRepository();
@@ -45,6 +64,61 @@ const productTemplate = `
 </li>
 `
 
+function seed() {
+    /** @type Product */
+    const first = {
+        id: "1",
+        name: "Смартфон DEXP A440 8 ГБ розовый",
+        thumbnailUrl: "./static/images/1.jpg",
+        price: 3200,
+        discount: null,
+        ram: 1,
+        storage: 8,
+        manufacturer: "DEXP"
+    };
+
+    /** @type Product */
+    const second = {
+        id: "2",
+        name: "Samsung Galaxy M52",
+        thumbnailUrl: "./static/images/2.jpg",
+        price: 40999,
+        discount: 4,
+        ram: 6,
+        storage: 256,
+        manufacturer: "Samsung"
+    };
+
+    /** @type Product */
+    const third = {
+        id: "3",
+        name: "Смартфон POCO F3 Черный",
+        thumbnailUrl: "./static/images/3.jpg",
+        price: 32999,
+        discount: null,
+        ram: 6,
+        storage: 128,
+        manufacturer: "POCO"
+    };
+
+    /** @type Product */
+    const fourth = {
+        id: "4",
+        name: "Смартфон POCO F3 Белый",
+        thumbnailUrl: "./static/images/4.jpg",
+        price: 34999,
+        discount: 6,
+        ram: 6,
+        storage: 128,
+        manufacturer: "POCO"
+    };
+
+    productRepository.add(first);
+    productRepository.add(second);
+    productRepository.add(third);
+    productRepository.add(fourth);
+}
+
 function render() {
     const rawProducts = document.getElementById("products");
 
@@ -66,4 +140,5 @@ function render() {
     }
 }
 
+seed();
 render();
